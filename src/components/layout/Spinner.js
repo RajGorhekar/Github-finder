3008205165e83978cbe5b667df02482204352154
RajGorhekar <rajgorhekar1234@gmail.com>
2020-08-07 23:03:22 +0530

    Without hooks

---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,9 @@
+import React from 'react';
+// import spinner from ',/spinner.gif'
+const Spinner = () => {
+	return <div align="center">
+		<h1>Loading ...</h1>
+	</div>;
+}
+
+export default Spinner;  
